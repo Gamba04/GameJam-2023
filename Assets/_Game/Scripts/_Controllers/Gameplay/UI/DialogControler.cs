7ac080fc618ff5dd376f4265497eba686169b9d1
StipGod <72163911+StipGod@users.noreply.github.com>
2023-02-04 20:48:56 -0500

    Dialog subtitles pog

--- Assets/_Game/Scripts/_Controllers/Gameplay/UI/DialogControler.cs
+++ Assets/_Game/Scripts/_Controllers/Gameplay/UI/DialogControler.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
 
+[Serializable]
+public class MessageInfo
+{
+    private string speaker;
+    private string message;
+    private Color colour;
+
+    public String GetSpeaker() { return speaker; }
+    public String GetMessage() { return message; }
+    public Color GetColor() { return colour; }
+}
 public class DialogControler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Text subtitle;
+    [SerializeField]
+    private Text speaker;
+    private string concat = "";
+    private int idx = 0;
+    private List<MessageInfo> dialogues;
+
+    public event Action onFinishedDialogue;
+
+    public void Start()
     {
-        
+        dialogues= new List<MessageInfo>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetDialogue(List<MessageInfo> str) 
+    {
+        dialogues = str;
+        StartDialogue();
+        
+    }
+     private void StartDialogue()
     {
         
+        if(idx < dialogues.Count)
+        {
+            StartCoroutine(DrawDialogue(dialogues[idx]));
+            onFinishedDialogue?.Invoke();
+            idx++;
+        }
+    }
+    private IEnumerator DrawDialogue(MessageInfo msgInfo)
+    {
+        speaker.text = msgInfo.GetSpeaker();
+        speaker.color = msgInfo.GetColor(); 
+
+        foreach (char c in msgInfo.GetMessage())
+        {
+            concat += c;
+            subtitle.text = concat;
+            yield return new WaitForSeconds(0.1f);
+        }
+        concat = "";
     }
 }
