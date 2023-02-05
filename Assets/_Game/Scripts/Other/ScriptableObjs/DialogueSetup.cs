f71a6dfff18f2ff706820e77231d55d35c5c6a9b
Gamba <juanca.quinterom@gmail.com>
2023-02-05 02:56:00 -0500

    MessageInfo spice

--- Assets/_Game/Scripts/Other/ScriptableObjs/DialogueSetup.cs
+++ Assets/_Game/Scripts/Other/ScriptableObjs/DialogueSetup.cs
@@ -1,9 +1,43 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public class MessageInfo
+{
+    [SerializeField, HideInInspector] private string name;
+
+    public string speaker;
+    public string message;
+    public Color color;
+
+    public void SetName()
+    {
+        int previewLength = Mathf.Min(message.Length, 5);
+
+        string messagePreview = message.Substring(0, previewLength);
+
+        name = $"{speaker}: {messagePreview}...";
+    }
+}
 
 [CreateAssetMenu(fileName = "DialogueSetup", menuName = "Setup/Dialogue Setup", order = 2)]
 public class DialogueSetup : ScriptableObject
 {
     [SerializeField]
     private List<MessageInfo> messages;
+
+    #region Editor
+
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        messages.ForEach(message => message.SetName());
+    }
+
+#endif
+
+    #endregion
+
 }
