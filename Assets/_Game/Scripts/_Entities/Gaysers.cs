440a20d3afe3d2da91e2c166f3666769863e2d19
StipGod <72163911+StipGod@users.noreply.github.com>
2023-02-05 01:17:04 -0500

    Gayser implemented

--- Assets/_Game/Scripts/_Entities/Gaysers.cs
+++ Assets/_Game/Scripts/_Entities/Gaysers.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Gaysers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
 
-    // Update is called once per frame
-    void Update()
+    private void GayserImpulse(Collider other)
     {
-        
+        IGaiserInteractable interactable = other.GetComponentInParent<IGaiserInteractable>();
+        if (interactable != null)
+        {
+            interactable.Impulse();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {   
+        GayserImpulse(other);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        GayserImpulse(other);
     }
 }
